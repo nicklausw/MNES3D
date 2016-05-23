ed02d759777b46bcc3537ffb11cf127d2f32db0b
nicklausw <nick@berlin.com>
2016-05-23 18:48:23 -0400

    simpler parsing

--- source/emulate.s
+++ source/emulate.s
@@ -23,38 +23,6 @@ get_byte:
   
   bx lr
 
-parse_opcode:
-  ldr r1, =opt_table
-  mov r3, #0
-  
- .lookup_loop:
-  ldrb r2, [r1]
-  cmp r0, r2
-  beq .lookup_done
-  
-  add r1, r1, #1
-  add r3, r3, #1
-  
-  cmp r3, #opt_table_end-opt_table
-  beq unknown_opcode
-  
-  b .lookup_loop
-  
- .lookup_done:
-  ldr r1, =opt_table_funcs
-  cmp r3, #0
-  beq .mult_done
-  
- .mult_loop:
-  add r1, r1, #4
-  sub r3, r3, #1
-  cmp r3, #0
-  bne .mult_loop
-  
- .mult_done:
-  ldr pc, [r1]
-  bx lr
-
 
 globll emulate
   stmfd  sp!, {lr}
@@ -72,9 +40,13 @@ globll emulate
   
   ldr r1, =opcode
   ldrb r0, [r1]
-  bl parse_opcode
   
-  ldmfd  sp!, {pc}
+  @cmp r0, #0x78 ; beq sei
+  @cmp r0, #0xd8 ; beq cld
+  @cmp r0, #0x20 ; beq jsr
+  
+  @b unknown_opcode
+  ldmfd  sp!, {lr}
   bx lr
 
 
@@ -84,6 +56,7 @@ sei:
   ldrb r0, [r1]
   orr r0, r0, #0b00000010
   strb r0, [r1]
+  ldmfd  sp!, {lr}
   bx lr
 
 cld:
@@ -91,15 +64,35 @@ cld:
   ldrb r0, [r1]
   and r0, r0, #0b00001000
   strb r0, [r1]
+  ldmfd  sp!, {lr}
   bx lr
 
+jsr:
+  bl get_byte
+  mov r1, r0
+  bl get_byte
+  lsl r0, #8
+  orr r0, r1
+  
+  sub r0, r0, #0xC000
+  add r0, r0, #0x10
+  ldr r1, =offset
+  str r0, [r1]
+  
+  mov r1, r0
+  ldr r0, =msg
+  bl iprintf
+  
+  ldmfd  sp!, {lr}
+  bx lr
+  
 .section .rodata
 opt_table_funcs:
-  .word sei, cld
+  .word sei, cld, jsr
 
 msg:
-  .ascii "opcode: 0x%02x\n\0"
+  .ascii "opcode: 0x%04x\n\0"
 
 opt_table:
-  .byte 0x78, 0xD8
+  .byte 0x78, 0xD8, 0x20, 0x00
 opt_table_end:
